523d38a86a83085be6586a86a3afdcdedecbe999
Kusov Kirill <kkusov@ozon.ru>
2019-02-07 22:06:19 +0300

    Update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 install_requires = [
     'gitpython == 2.1.7',
     'prettytable == 0.7.2',
-    'requests == 2.18.4',
+    'requests == 2.20.0',
     'configparser == 3.5.0',
     'future == 0.16.0'
 ]
